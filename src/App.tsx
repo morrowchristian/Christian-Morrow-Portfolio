f23187e96539cadc5ea6297a599ad089dfec1f48
morrowchristian <morrowchristian@icloud.com>
2025-12-26 21:22:14 -0500

    chore: add missing Footer component to App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,34 @@
+import { useEffect } from "react";
 import Navbar from "./components/Navbar";
-import Hero from './sections/Hero';
+import Hero from "./sections/Hero";
 import About from "./sections/About";
 import Skills from "./sections/Skills";
 import Projects from "./sections/Projects";
 import Contact from "./sections/Contact";
+import Footer from "./components/Footer";
 import ScrollToTop from "./components/ScrollToTop";
 
 const App = () => {
+  useEffect(() => {
+    document.documentElement.style.scrollBehavior = "smooth";
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <>
-       <div id="progress-bar"></div>
+      <div id="progress-bar" aria-hidden="true"></div>
+      
       <Navbar />
-      <Hero />
-      <About />
-      <Skills />
-      <Projects />
-      <Contact />
+      
+      <main>
+        <Hero />
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
+      </main>
 
+      <Footer />
       <ScrollToTop />
     </>
   );
